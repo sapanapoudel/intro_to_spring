9ae7e82b5e34831739fda99d8b6bd38787978732
sapanapoudel <sapana.poudel17@outlook.com>
2019-08-19 16:13:02 -0700

    Setup

---
+++ src/main/java/com/poudel/introToSpring/IntroToSpringApplication.java
@@ -0,0 +1,13 @@
+package com.poudel.introToSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroToSpringApplication.class, args);
+	}
+
+}
